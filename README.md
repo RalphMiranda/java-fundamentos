28d7c84902164eb33934e64fc87be3c5c42309d6
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2025-05-06 11:34:21 -0300

    Aula 3.3 - Entendendo o conceito de cast

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ O deste projeto é compreender os princípios fundamentais da plataforma Java co
 
 ## Change log
 
-- Aula 2.2 - Entendento o conceito dos packages
-  - "Olá, Mundo!"
+2. Primeiros contatos com Java
+  - Aula 2.2 - Entendento o conceito dos packages
+    - "Olá, Mundo!"
+  - Aula 2.4 - Escrevendo mensagens no console
+    - ```System.out.println``` e ```System.out.print```
+  - Desafio de Código - 2. Primeiros contatos com Java
+
+3. Lidando com variáveis e tipos de dados
+  - Aula 3.2 - Variáveis, streams e o Scanner
+  - Aula 3.3 - Entendendo o conceito de cast
+    - ```int numero = Integer.parseInt(scanner.nextLine());```
--- src/br/com/ralph/tw/main/Main.java
+++ src/br/com/ralph/tw/main/Main.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
 
-        System.out.println("Olá, Mundo!"); // Imprime mensagem já com quebra de linha
-        System.out.print("Olá mundo de novo \n"); // Imprime mensagem sem quebra de linha, por isso, o uso do metacactere "\n"
-        System.out.print("Olá mundo mais uma vez \n");
+        System.out.print("Digite um número: ");
+        int numero = Integer.parseInt(scanner.nextLine());
 
-        System.out.println(nome);
+        System.out.println(numero);
+
         scanner.close();
     }
 }
