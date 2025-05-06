4e9352582112f2db9201a5cb16b64ea56d1688b8
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2025-05-06 10:56:26 -0300

    Desafio de Código -  2. Primeiros contatos com Java

---
+++ src/br/com/ralph/tw/desafios/Desafio2.java
@@ -0,0 +1,23 @@
+package br.com.ralph.tw.desafios;
+
+/* DESAFIO DE CÓDIGO - 2. PRIMEIROS CONTATOS COM JAVA
+Complete o código no ponto indicado, de forma que ele exiba no console as mensagens "Olá Mundo!" e "Bem-vindo a formação Java da Treinaweb!".
+
+Entrada de dados
+
+- Não há nenhuma entrada
+
+Saída esperada
+
+"Olá Mundo!
+Bem-vindo a formação Java da Treinaweb!"
+ */
+
+public class Desafio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+
+        System.out.println("Bem-vindo a formação Java da Treinaweb!");
+    }
+}
