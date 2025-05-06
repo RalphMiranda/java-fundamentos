7fdebed1bb0c472ab4306b3c67ad9ae0e3ea448a
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2025-05-06 11:13:00 -0300

    Aula 3.2 - Variáveis, streams e o Scanner

--- src/br/com/ralph/tw/main/Main.java
+++ src/br/com/ralph/tw/main/Main.java
@@ -1,10 +1,18 @@
 package br.com.ralph.tw.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!"); // Imprime mensagem já com quebra de linha
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
 
+        System.out.println("Olá, Mundo!"); // Imprime mensagem já com quebra de linha
         System.out.print("Olá mundo de novo \n"); // Imprime mensagem sem quebra de linha, por isso, o uso do metacactere "\n"
-        System.out.print("Olá mundo mais uma vez");
+        System.out.print("Olá mundo mais uma vez \n");
+
+        System.out.println(nome);
+        scanner.close();
     }
 }
