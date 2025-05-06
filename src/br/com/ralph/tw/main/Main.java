bd5990b2f3fec1c7b9ec090635cfd544a384fbea
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2025-05-06 10:19:21 -0300

    Aula 2.4 - Escrevendo mensagens no console

--- src/br/com/ralph/tw/main/Main.java
+++ src/br/com/ralph/tw/main/Main.java
@@ -2,6 +2,9 @@ package br.com.ralph.tw.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+        System.out.println("Olá, Mundo!"); // Imprime mensagem já com quebra de linha
+
+        System.out.print("Olá mundo de novo \n"); // Imprime mensagem sem quebra de linha, por isso, o uso do metacactere "\n"
+        System.out.print("Olá mundo mais uma vez");
     }
 }
